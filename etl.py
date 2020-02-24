b571ed35fd028de531c34fbef607742d4d1aa007
harini-akula <harini.akula01@gmail.com>
2020-02-24 00:29:19 +0000

    feat: Read song-data json files from given path
    Read song-data json files from a local directory and defined schema

---
+++ etl.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.types as t
+
+def process_song_data(spark, input_data, output_data):
+    # get filepath to song data file
+    song_data = input_data + 'song_data/*/*/*/*.json'
+    
+    # define schema for song data file
+    song_schema = t.StructType([
+        t.StructField("artist_id", t.StringType(), True),
+        t.StructField("artist_latitude", t.DecimalType(11, 7), True),
+        t.StructField("artist_location", t.StringType(), True),
+        t.StructField("artist_longitude", t.DecimalType(11, 7), True),
+        t.StructField("artist_name", t.StringType(), True),
+        t.StructField("duration", t.DecimalType(11, 7), True),
+        t.StructField("num_songs", t.IntegerType(), True),
+        t.StructField("song_id", t.StringType(), True),
+        t.StructField("title", t.StringType(), True),
+        t.StructField("year", t.ShortType(), True)
+    ])
+    
+    # read song data file using schema
+    df = spark \
+        .read \
+        .json(song_data, song_schema)
+    
+    
+def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
+
+
+def main():
+    spark = create_spark_session()
+    spark.sparkContext.setLogLevel("ERROR")
+    input_data = '/home/workspace/data/more/'
+    output_data = 'output/'
+    
+    process_song_data(spark, input_data, output_data)    
+
+
+if __name__ == "__main__":
+    main()
